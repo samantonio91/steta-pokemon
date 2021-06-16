4dc9b83f7ee992ecab410c6d33f9225cc2f4863e
samantonio9 <sam.antonio.91@gmail.com>
2021-06-16 11:46:50 -0500

    se crean vistas en elementos

--- src/components/PokemonCardInput.js
+++ src/components/PokemonCardInput.js
@@ -6,13 +6,7 @@ const Pokedex = ({ name, abilities, weight, pokemonImage, addNewPokemon, state }
 
   return (
     <div className="pokemonCard">
-      <h1>Pokedex</h1>
-      <h2>Pokemon name: {name}</h2>
-      <p>Weight: {weight}</p>
-      <img src={pokemonImage} alt="Pokemon IMG" />
-      <button onClick={() => addNewPokemon({ name, abilities, weight, pokemonImage })}>Save pokemon</button>
-      <Link to="/pokemonList"> Go to pokemon list</Link>
-      <Link to="/createPokemon"> Go to pokemon list</Link>
+
     </div>
   )
 }
